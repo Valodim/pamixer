f3acd91252c768410e3b3fec9456a334a0e5efc0
Valodim <valodim@mugenguild.com>
2011-03-04 00:42:11 +0100

    help screen mock-up

--- screens/ScreenHelp.py
+++ screens/ScreenHelp.py
@@ -4,8 +4,26 @@ class ScreenHelp():
     def __init__(self):
         return
 
-    def draw(self, win):
+    def layout(self, win):
+        self.win = win
+
+        # initial redraw
+        self.redraw()
+
+    def redraw(self, recurse = False):
+        if self.win is None:
+            return
+
+        win = self.win
+
+        win.erase()
+        win.move(3, 7)
+
+        win.addstr("Hello, World!")
+
+        win.refresh()
+
         return
 
     def key_event(self, event):
-        return
+        return False
