8a19ddf79febce5f7f685e7c63d53a27ec974589
Valodim <valodim@mugenguild.com>
2011-03-16 23:11:41 +0100

    use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pamixer',
-      version='0.1',
+      version='0.1.1',
       description='PulseAudio Curses Mixer Application',
       author='Valodim',
       author_email='valodim@mugenguild.com',
       url='http://github.com/valodim/pamixer',
-      download_url='http://datatomb.de/~valodim/pamixer-0.1.tar.gz',
+      download_url='http://datatomb.de/~valodim/pamixer-0.1.1.tar.gz',
       packages=['pamixer', 'pamixer.classes', 'pamixer.screens'],
       scripts=['bin/pamixer'],
-      requires=['libpulseaudio(>=0.9.22)']
+      install_requires=['libpulseaudio >=0.9.22','argparse']
      )
