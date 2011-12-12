01df3110f88fa5a2a1ac6142ec779a3316c2fcea
Vincent Breitmoser <valodim@mugenguild.com>
2011-12-13 00:24:44 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name='pamixer',
-      version='0.1.3',
+      version='0.1.4',
       description='PulseAudio Curses Mixer Application',
       author='Valodim',
       author_email='valodim@mugenguild.com',
       url='http://github.com/valodim/pamixer',
       packages=['pamixer', 'pamixer.classes', 'pamixer.screens'],
       scripts=['bin/pamixer'],
-      requires=['libpulseaudio(>=0.9.22)','argparse'],
-      install_requires=['libpulseaudio >=0.9.22.1','argparse']
+      requires=['libpulseaudio(>=1.1)','argparse'],
+      install_requires=['libpulseaudio >=1.1','argparse']
      )
